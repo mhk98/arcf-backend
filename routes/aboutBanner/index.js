5fa1b8ddd78ea0db8c6a128831ec005dbd7411bb
mhk98 <mohsinkabirseo@gmail.com>
2023-12-11 23:50:13 +0600

    update today

--- routes/aboutBanner/index.js
+++ routes/aboutBanner/index.js
@@ -5,7 +5,11 @@ const auth = require("../../middlewares/auth");
 
 router.get("/", aboutBanner.getAllaboutBanner);
 // router.get("/:id", auth("user", "admin"), report.singleReport);
-router.post("/create-aboutBanner", upload.single("file"), aboutBanner.createaboutBanner);
+router.post(
+  "/create-banner",
+  upload.single("file"),
+  aboutBanner.createaboutBanner
+);
 router.delete("/:id", aboutBanner.deleteaboutBanner);
 router.put("/:id", upload.single("file"), aboutBanner.updateaboutBanner);
 
