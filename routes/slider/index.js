ce621bff6fe11e0c749539e6dcd4e3d44b930fb7
mhk98 <mohsinkabirseo@gmail.com>
2023-12-13 22:28:45 +0600

    udpate

--- routes/slider/index.js
+++ routes/slider/index.js
@@ -3,7 +3,7 @@ const slider = require("../../controllers/slider/slider.controller");
 const { upload } = require("../../middlewares/upload");
 
 router.get("/", slider.getAllslider);
-// router.get("/:id", auth("user", "admin"), report.singleReport);
+router.get("/:id", slider.singleslider);
 router.post("/create-slider", upload, slider.createslider);
 router.delete("/:id", slider.deleteslider);
 router.patch("/:id", upload, slider.updateslider);
